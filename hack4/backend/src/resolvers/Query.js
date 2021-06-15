c10246f00957def878679c8437b60f11c8aebb42
IMEVA <55312900+b06705039@users.noreply.github.com>
2021-06-15 11:00:05 +0800

    statsCount

--- hack4/backend/src/resolvers/Query.js
+++ hack4/backend/src/resolvers/Query.js
@@ -4,8 +4,21 @@ const Query = {
             let u1 = collections.filter((person) => {
                 return person.location.description.includes(locationKeywords);
             })
+            if(!severity) return u1
             const result = u1.filter((person)=>{ return person.severity >= severity });
             return result
+        }catch{
+            return null
+        }
+    },
+    statsCount(parent, { severity, locationKeywords }, { collections } , info) {
+        try{
+            let u1 = collections.filter((person) => {
+                return person.location.description.includes(locationKeywords);
+            })
+            if(!severity) return u1.length
+            const result = u1.filter((person)=>{ return person.severity >= severity });
+            return result.length
         }catch{
             return null
         }
