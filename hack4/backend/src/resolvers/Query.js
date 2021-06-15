9c9d7063236a38a06db1fc75f553b8c206f7e114
IMEVA <55312900+b06705039@users.noreply.github.com>
2021-06-15 10:39:54 +0800

    query

--- hack4/backend/src/resolvers/Query.js
+++ hack4/backend/src/resolvers/Query.js
@@ -1,14 +1,14 @@
 const Query = {
     people(parent, { severity, locationKeywords }, { collections } , info) {
-      if (!severity || !locationKeywords) {
-        return collections;
-      }
-      console.log("in Query Persons: ", collections)
-  
-      let u1 = collections.filter((person) => {
-        return person.location.description.includes(locationKeywords);
-      })
-      return u1.filter((person)=>{ return person.severity >= severity });
+        try{
+            let u1 = collections.filter((person) => {
+                return person.location.description.includes(locationKeywords);
+            })
+            const result = u1.filter((person)=>{ return person.severity >= severity });
+            return result
+        }catch{
+            return null
+        }
     },
 }
 
